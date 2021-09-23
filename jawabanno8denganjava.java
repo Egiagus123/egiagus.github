8211579544d9650298a39e80ea6cf0fc80551655
Egiagus123 <91176764+Egiagus123@users.noreply.github.com>
2021-09-23 11:50:24 +0700

    Add files via upload

---
+++ jawabanno8denganjava.java
@@ -0,0 +1,29 @@
+package bhp.erp.model;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+import org.compiere.model.MAttachment;
+public class jawabanno8denganjava {
+		
+
+	//Nomor 9 dengan java
+	public static void main(String[] args) {
+		
+	  int[] angka = {1,2,3,4,5};
+      int sum3 = 0; 
+      int sum5 = 0;
+      for (int i = 1; i <= 3; i++) {
+    	  sum3 = sum3+i;
+      }
+      System.out.println("sample case 1 : " + sum3);
+      
+      for (int i = 1; i <= 5; i++) {
+    	  sum5 = sum5+i;
+      }
+      System.out.println("sample case 2 : " + sum5);
+	}
+}
+
